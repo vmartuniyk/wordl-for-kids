561d6f2b77a3dca137c9d4173bfe28a9fc73db2f
vmartuniyk <martuniyk93@ukr.net>
2023-02-18 10:28:52 +0200

    make grid with Apline

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,4 @@
 let grid = document.querySelector('#game');
 // the number of guesees (3)
-let guessesAllow = 2;
-let wordLength = 6;
-
-//generate 3 rows
-let fragment = document.createDocumentFragment();
-
-let generateGrid = () => {
-
-    Array.from({length: guessesAllow}).forEach(() =>{
-        let row = document.createElement('div');
-
-        row.classList.add('row');
-        Array.from({length: wordLength}).forEach(() => {
-            let tile = document.createElement('div');
-
-            tile.classList.add('tile');
-            row.appendChild(tile);
-        })
-
-        fragment.appendChild(row);
-    })
-
-    grid.appendChild(fragment);
-
-}
-
-generateGrid()
+let guessesAllow = 3;
+let wordLength = 3;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,26 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="//unpkg.com/alpinejs" defer></script>
+    @vite(['resources/css/app.css'])
+{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
     <title>Try Cat</title>
 </head>
 <body>
-    <div id="game">
-{{--            <div class="row">--}}
-{{--                <div class="tile"></div>--}}
-{{--                <div class="tile"></div>--}}
-{{--                <div class="tile"></div>--}}
-{{--            </div>--}}
-{{--            <div class="row">--}}
-{{--                <div class="tile"></div>--}}
-{{--                <div class="tile"></div>--}}
-{{--                <div class="tile"></div>--}}
-{{--            </div>--}}
-{{--            <div class="row">--}}
-{{--                <div class="tile"></div>--}}
-{{--                <div class="tile"></div>--}}
-{{--                <div class="tile"></div>--}}
-{{--            </div>--}}
+    <div id="game" x-data="{guessesAllow: 3, wordLength: 3 }">
+        <template x-for="row in Array.from({ length: guessesAllow })">
+            <div class="row">
+                <template x-for="tile in Array.from({length: wordLength})">
+                    <div class="tile"></div>
+                </template>
+            </div>
+        </template>
     </div>
 </body>
 </html>
