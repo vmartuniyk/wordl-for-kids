368809db0869771d7d1ee086d11be2a01bb537fc
vmartuniyk <martuniyk93@ukr.net>
2023-02-19 19:50:08 +0200

    Build assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,16 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <script src="//unpkg.com/alpinejs" defer></script>
+{{--    @vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+    @production
+        @php
+            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+        @endphp
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @endproduction
+{{--    <script src="//unpkg.com/alpinejs" defer></script>--}}
     <title>Try Cat</title>
 </head>
 <body>
